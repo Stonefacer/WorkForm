31d07fb8ad0c2366a4b573dba2389397a312c237
Stonefacer <toxagudkov1994@gmail.com>
2017-06-19 18:47:10 +0300

    Unlimited autocompletes

--- Wok_Form/Controllers/HomeController.cs
+++ Wok_Form/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,6 +20,30 @@ namespace Wok_Form.Controllers {
                 new Student() { Id = "6", Name = "Hugh Darroy", Group = "NO-21-1" },
                 new Student() { Id = "7", Name = "Elisa Cassan", Group = "NO-22-1" },
                 new Student() { Id = "8", Name = "El Capitano", Group = "NO-23-1" },
+            };
+
+        private static Student[] _data0 = new Student[] {
+                new Student() { Id = "0", Name = "Be The Bee0", Group = "NO-15-1" },
+                new Student() { Id = "1", Name = "Soul Goodman0", Group = "NO-16-1" },
+                new Student() { Id = "2", Name = "Mr. Freemen0", Group = "NO-17-1" },
+                new Student() { Id = "3", Name = "Isaac Sandaval0", Group = "NO-18-1" },
+                new Student() { Id = "4", Name = "William Taggart0", Group = "NO-19-1" },
+                new Student() { Id = "5", Name = "David Sharif0", Group = "NO-20-1" },
+                new Student() { Id = "6", Name = "Hugh Darroy0", Group = "NO-21-1" },
+                new Student() { Id = "7", Name = "Elisa Cassan0", Group = "NO-22-1" },
+                new Student() { Id = "8", Name = "El Capitano0", Group = "NO-23-1" },
+            };
+
+        private static Student[] _data1 = new Student[] {
+                new Student() { Id = "0", Name = "Be The Bee1", Group = "NO-15-1" },
+                new Student() { Id = "1", Name = "Soul Goodman1", Group = "NO-16-1" },
+                new Student() { Id = "2", Name = "Mr. Freemen1", Group = "NO-17-1" },
+                new Student() { Id = "3", Name = "Isaac Sandaval1", Group = "NO-18-1" },
+                new Student() { Id = "4", Name = "William Taggart1", Group = "NO-19-1" },
+                new Student() { Id = "5", Name = "David Sharif1", Group = "NO-20-1" },
+                new Student() { Id = "6", Name = "Hugh Darroy1", Group = "NO-21-1" },
+                new Student() { Id = "7", Name = "Elisa Cassan1", Group = "NO-22-1" },
+                new Student() { Id = "8", Name = "El Capitano1", Group = "NO-23-1" },
             };
 
         public ActionResult Index() {
@@ -43,9 +68,29 @@ namespace Wok_Form.Controllers {
             return Json(result);
         }
 
+        public ActionResult GetList0(string value)
+        {
+            value = value.ToLower();
+            var result = _data0.Where(x => x.Name.ToLower().StartsWith(value));
+            return Json(result);
+        }
+
+        public ActionResult GetList1(string value)
+        {
+            value = value.ToLower();
+            var result = _data1.Where(x => x.Name.ToLower().StartsWith(value));
+            return Json(result);
+        }
+
         public string SubmitForm()
         {
-            return Request.Form["students"]?.Replace("},{", "}<br>{") ?? "NULL";
+            var res = new StringBuilder();
+            res.Append(Request.Form["students"]?.Replace("},{", "}<br>{") ?? "NULL");
+            res.Append("<br>---------------------<br>");
+            res.Append(Request.Form["students0"]?.Replace("},{", "}<br>{") ?? "NULL");
+            res.Append("<br>---------------------<br>");
+            res.Append(Request.Form["students1"]?.Replace("},{", "}<br>{") ?? "NULL");
+            return res.ToString();
         }
     }
 }
--- Wok_Form/Scripts/Autocomplete.js
+++ Wok_Form/Scripts/Autocomplete.js
@@ -1,119 +1,147 @@
 ﻿"use strict";
 
-window.app = {};
+window.app = {
+    autocompleteInputs: [],
+}
 
-window.app.addedStudents = [];
+function AutoCompleteObject(input, index) {
 
-window.app.addStudent = function (student,addOption) {
-    var span = $('<span class="label label-default">' + student.Name + '&nbsp;(' + student.Group + ')' + '<span class="remove glyphicon glyphicon-remove-sign glyphicon-white"></span></span>');
-    span.click(function () {
-        window.app.removeStudent(student);
-        span.remove();
-    });
-    window.app.container.append(span);
-    window.app.addedStudents.push(student);
-    if (addOption === false) {
-        return;
-    }
-    var option = $('<option selected></option>');
-    option.val(JSON.stringify(student));
-    option.attr("data-id", student.Id);
-    window.app.studentsList.append(option);
-};
+    // fields
 
-window.app.removeStudent = function (student) {
-    //window.app.addedStudents[student.Id] = undefined;
-    var id = window.app.addedStudents.indexOf(student);
-    delete window.app.addedStudents[id];
-    window.app.studentsList.children("option[value='" + JSON.stringify(student) + "']").remove();
-};
+    this.input = $(input);
+    this.addedStudents = [];
+    this.url = this.input.attr("data-autocomplete-url");
+    this.minimalLength = this.input.attr("data-autocomplete-minimal-length");
+    this.container = $("#" + this.input.attr("data-autocomplete-labels-container-id"));
+    this.studentsList = $("#" + this.input.attr("data-autocomplete-select-id"));
+    this.studentGroup = $("#" + this.input.attr("data-autocomplete-group-input-id"));
+    this.addButton = $("#" + this.input.attr("data-autocomplete-add-button-id"));
+    this.index = index;
 
-window.app.containStudent = function (student) {
-    for (var i = 0; i < window.app.addedStudents.length; i++) {
-        if (typeof window.app.addedStudents[i] === 'undefined') {
-            continue;
-        }
-        if (window.app.addedStudents[i].Name == student.Name && window.app.addedStudents[i].Group == student.Group) {
-            return true;
-        }
-    }
-    return false;
-};
+    // functions
 
-window.app.createStudent = function(Id, Name, Group){
-    return { Id: Id, Name: Name, Group: Group };
-};
-
-window.app.loadStudents = function (selectElement) {
-    var options = $(selectElement).children("option[selected]");
-    for (var i = 0; i < options.length; i++) {
-        var student = JSON.parse($(options[i]).val());
-        if (typeof student.Id == 'undefined' || typeof student.Name == 'undefined' || typeof student.Group == 'undefined') {
-            continue;
-        }
-        window.app.addStudent(student, false);
-    }
-};
-
-(function(app){
-    $(function ($) {
-        var form = $("form[data-autocomlete-url]")[0];
-        app.url = $(form).attr("data-autocomlete-url");
-        app.minimalLength = $(form).attr("data-autocomlete-minimal-length");
-        app.container = $("#selected-students-visual");
-        app.studentsList = $("#selected-students");
-        app.studentName = $("#student");
-        app.studentGroup = $("#group");
-        app.selectedStudents = $("#students");
-        var inputId = $(form).attr("data-autocomlete-input-id");
-        var input = $("#" + inputId);
-        app.loadStudents(app.studentsList);
-        $(input).autocomplete(
-        {
-            minLength: app.minimalLength,
-            source: function (request, response) {
-                app.selected = undefined;
-                $.ajax({
-                    method: "POST",
-                    url: app.url,
-                    data: {
-                        value: request.term,
-                    },
-                    success: function (data) {
-                        for (var i = 0; i < data.length; i++) {
-                            data[i].label = data[i].Name;
-                        }
-                        response(data);
-                    },
-                    error: function (result, textStatus, errorThrown) {
-                        console.log(textStatus);
-                        console.log(errorThrown);
-                    }
-                });
-            },
-            select: function (event, ui) {
-                app.selected = ui.item;
-                app.studentName.val(ui.item.Name);
-                app.studentGroup.val(ui.item.Group);
-                return false;
-            }
+    this.addStudent = function (student, addOption) {
+        var span = $('<span class="label label-default">' + student.Name + '&nbsp;(' + student.Group + ')' + '<span class="remove glyphicon glyphicon-remove-sign glyphicon-white"></span></span>');
+        span.attr("data-input-index", this.index);
+        span.click(function () {
+            var index = $(this).attr("data-input-index");
+            var owner = window.app.autocompleteInputs[index];
+            owner.removeStudent(student);
+            span.remove();
         });
-        $("#add-student").click(function (e) {
-            var currentStudent = app.selected;
-            var realGroup = app.studentGroup.val();
-            if (typeof app.selected == 'undefined' || app.selected.Group != realGroup) {
-                currentStudent = app.createStudent(0, app.studentName.val(), realGroup);
-                if (currentStudent.Name == "" || currentStudent.Group == "") {
-                    return;
-                }
+        this.container.append(span);
+        this.addedStudents.push(student);
+        if (addOption === false) {
+            return;
+        }
+        var option = $('<option selected></option>');
+        option.val(JSON.stringify(student));
+        option.attr("data-id", student.Id);
+        this.studentsList.append(option);
+    };
+
+    this.removeStudent = function (student) {
+        //window.app.addedStudents[student.Id] = undefined;
+        var id = this.addedStudents.indexOf(student);
+        delete this.addedStudents[id];
+        this.studentsList.children("option[value='" + JSON.stringify(student) + "']").remove();
+    };
+
+    this.containStudent = function (student) {
+        for (var i = 0; i < this.addedStudents.length; i++) {
+            if (typeof this.addedStudents[i] === 'undefined') {
+                continue;
             }
-            if (app.containStudent(currentStudent)) {
+            if (this.addedStudents[i].Name == student.Name && this.addedStudents[i].Group == student.Group) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    this.createStudent = function (Id, Name, Group) {
+        return { Id: Id, Name: Name, Group: Group };
+    };
+
+    this.loadStudents = function (selectElement) {
+        var options = $(selectElement).children("option[selected]");
+        for (var i = 0; i < options.length; i++) {
+            var student = JSON.parse($(options[i]).val());
+            if (typeof student.Id == 'undefined' || typeof student.Name == 'undefined' || typeof student.Group == 'undefined') {
+                continue;
+            }
+            this.addStudent(student, false);
+        }
+    };
+
+    var owner = this;
+
+    var settings = {
+        minLength: this.minimalLength,
+        source: function (request, response) {
+            var index = this.element.data("autocomplete-index");
+            var owner = window.app.autocompleteInputs[index];
+            owner.selected = undefined;
+            $.ajax({
+                method: "POST",
+                url: owner.url,
+                data: {
+                    value: request.term,
+                },
+                success: function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        data[i].label = data[i].Name;
+                    }
+                    response(data);
+                },
+                error: function (result, textStatus, errorThrown) {
+                    console.log(textStatus);
+                    console.log(errorThrown);
+                }
+            });
+        },
+        select: function (event, ui) {
+            var index = $(event.target).data("autocomplete-index");
+            var owner = window.app.autocompleteInputs[index];
+            owner.selected = ui.item;
+            owner.input.val(ui.item.Name);
+            owner.studentGroup.val(ui.item.Group);
+            return false;
+        }
+    };
+
+    $(this.input).autocomplete(settings);
+
+    $(this.addButton).click(function (e) {
+        var index = $(this).data("autocomplete-index");
+        var owner = window.app.autocompleteInputs[index];
+        var currentStudent = owner.selected;
+        var realGroup = owner.studentGroup.val();
+        if (typeof owner.selected == 'undefined' || owner.selected.Group != realGroup) {
+            currentStudent = owner.createStudent(0, owner.input.val(), realGroup);
+            if (currentStudent.Name == "" || currentStudent.Group == "") {
                 return;
             }
-            app.studentName.val("");
-            app.studentGroup.val("");
-            app.addStudent(currentStudent);
-            app.selected = undefined;
-        });
+        }
+        if (owner.containStudent(currentStudent)) {
+            return;
+        }
+        owner.input.val("");
+        owner.studentGroup.val("");
+        owner.addStudent(currentStudent);
+        owner.selected = undefined;
+    });
+
+    this.loadStudents(this.studentsList);
+    this.input.data("autocomplete-index", this.index);
+    this.addButton.data("autocomplete-index", this.index);
+}
+
+(function (app) {
+    $(function ($) {
+        var inputs = $("input[data-autocomplete-url]");
+        for (var i = 0; i < inputs.length; i++) {
+            app.autocompleteInputs[i] = new AutoCompleteObject(inputs[i], i);
+        }
     });
 })(window.app);
